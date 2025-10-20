d9f5a278a0a7913134dfa06214ee61dcc2b29b8a
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-10-20 19:38:00 +0500

    check bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import SignUp from "./components/signUp"
 function App() {
   return (
     <>
-      
+      <h1>Hello</h1>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import SignUp from './components/signUp/index.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SignUp />
+    <App /> 
   </StrictMode>,
 )
